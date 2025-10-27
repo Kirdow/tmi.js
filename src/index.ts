776e90a3f182a5c820696e542e2d9e38388942d8
Kirdow <10155603+Kirdow@users.noreply.github.com>
2025-10-27 14:10:00 +0100

    export all types from index.ts
      Add type exports for ChatUserstate and all other userstate types
      so they can be imported from 'tmi.js' in consuming projects

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
 import Client from './lib/Client.js';
 
+// Export the Client class
 export { Client };
 export { Client as client };
 export default { Client, client: Client };
+
+// Re-export all types
+export type {
+	ChannelName,
+	OutgoingTags,
+	ClientOptions,
+	BadgeInfo,
+	Badges,
+	GlobalUserstate,
+	Userstate,
+	DeleteUserstate,
+	ChatUserstate,
+	SubGiftUserstate,
+	SubUserstate,
+	AnonSubGiftUserstate,
+	SubMysteryGiftUserstate,
+	PrimePaidUpgradeUserstate,
+	GiftPaidUpgradeUserstate,
+	RaidUserstate,
+	RitualUserstate,
+	BitsBadgeTierUserstate
+} from './types.js';
+
+// Export the Options namespace
+export { Options } from './types.js';
+
+// Re-export Logger type
+export type { LogLevel } from './lib/Logger.js';
